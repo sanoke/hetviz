db0f5b157649e841e990d9812a467e898b75ff5f
Sarah Anoke <sanoke527@gmail.com>
2017-04-24 20:27:45 -0400

    Work on installing w devtools

--- R/runShinyApp.R
+++ R/runShinyApp.R
@@ -14,15 +14,8 @@ hetviz <- function() {
     stop("Could not find directory containing GUI. Try re-installing `hetviz`.",
          call. = FALSE)
   }
-  #helperFiles <- c("covarProfiles-plotFcn.R",
-  #                 "forestPlot-plotFcns.R",
-  #                 "subgrouProfiles-plotFcn.R",
-  #                 "summaryFcns.R",
-  #                 "vizByCovar-plotFcn.R",
-  #                 "vizBySubgroup-plotFcns.R")
-  #print(helperFiles)
-  #for(h in helperFiles)
-  #  print(h)
-  #  source(h, local = TRUE)
+
+  devtools::load_all(pkg = system.file(package = "hetviz"))
+  
   shiny::runApp(appDir, display.mode = "normal")
 }
